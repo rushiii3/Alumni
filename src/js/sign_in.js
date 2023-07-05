df093ba34cef04181e24692b910b4e7ca143f010
hrushii <hrushiop@gmail.com>
2023-07-05 22:01:09 +0530

    done home page

---
+++ src/js/sign_in.js
@@ -0,0 +1,14 @@
+$('.pass_open_eye').hide();
+$('.pass_icon').on('click',function(){
+    if('password' == $('#password').attr('type')){
+        $('#password').prop('type', 'text');
+        $('.pass_open_eye').show();
+        $('.pass_close_eye').hide();
+
+   }else{
+        $('#password').prop('type', 'password');
+        $('.pass_open_eye').hide();
+        $('.pass_close_eye').show();
+   }
+})
+console.log("yes");
---
+++ src/main/navbar2.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light" id="navigation">
+                <div class="container">
+                <a class="navbar-brand" href="#">
+                <img src="../img/logo11-removebg-preview.png" alt="college_logo" style="" id="navBarLogo">
+                    </a>
+                    <button type="button" id="sidebarCollapse" class="btn btn-dark  ml-auto">
+                    <i class="bi bi-list"></i>
+                    </button>
+            </nav>
