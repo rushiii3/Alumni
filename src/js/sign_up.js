6d3b75a36777fb5433ad421b44b3c622963d6b5b
Aaditya Pal <aaditya.sp@somaiya.edu>
2023-07-11 23:40:03 +0530

    email functionality added in  signup

--- src/js/sign_up.js
+++ src/js/sign_up.js
@@ -23,6 +23,7 @@ let failed_modal_message_para=document.getElementById("failed_registration_messa
    let first_name="",middle_name="",last_name="",dob="",linkedin_address="",phone_number="",email="";
    let errorInputFieldsArray=new Array();
    let generated_OTP="";
+   let email_sent_to_address="";
 
    //PAGE 2.
    let bachelors_degree="",bachelors_admission_year="",bachelors_degree_college_name="";
@@ -216,7 +217,7 @@ $('#previous_page_4').on('click',function(){
 verify_email_button.addEventListener('click',function(){
     //displayEmailOTPDiv();
     generateOTP();
-    sendEmail();
+    sendEmail(email_input.value);
     verify_otp_div.removeAttribute("hidden");
     //todo:generate the otp and send the email
 });
@@ -355,14 +356,25 @@ verify_otp_div.removeAttribute("hidden");
   }
 
   //5.send the email with OTP
-  function sendEmail(){
+  function sendEmail(email_to){
+    email_sent_to_address=email_to;
+   let body_of_email=`<div id='emailContent' style='max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ccc; background-color: #f9f9f9;'><div style='max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ccc; background-color: #f9f9f9;'><table style='height: 100%; width: 100%; background-color: #f5f6f8;'><tbody><tr><td valign='top' class='edimg' style='padding: 5px; box-sizing: border-box; text-align: center;'><img src='https://github.com/rushiii3/Book_A_Slot/blob/main/src/img/logo11.jpeg?raw=true' alt='Image' width='108' style='border-width: 0px; border-style: none; max-width: 108px; width: 100%;'></td> </tr><tr><td valign='top' class='edtext' style='padding: 5px; text-align: left; color: #5f5f5f; font-size: 15px; '><h2 class='text-center' style='text-align: center;'><strong>V.G. Vaze College of Arts, Science and Commerce (Autonomous)</strong></h2></td></tr><tr><td valign='top' class='edtext' style='background-color: #ffffff;padding: 32px; text-align: left; color: #5f5f5f; font-size: 15px; '><p class='text-center' style='text-align: center; margin: 0px; padding: 0px;'>Greetings <strong> ${email_to}</strong>!</p><br><p class='text-center' style='text-align: center; margin: 0px; padding: 0px;'>You have requested to verify this email address </p><br><br><p class='text-center' style='text-align: center; margin: 0px; padding: 0px;'>The requested OTP is :  <strong>${generated_OTP}</strong>.Please do not share this OTP with anyone. </p> <p style='margin: 0px; font-size: 15px;margin-top:4rem ;'>Vaze Alumni App</p></td></tr><tr><td valign='top' class='edtext' style='padding: 20px; text-align: left; color: #5f5f5f; font-size: 15px;'><p class='text-center' style='line-height: 1.75em; text-align: center; margin: 0px; padding: 0px;'><span style='font-size: 11px;'>If you no longer wish to receive mail from us, you can <a href='{unsubscribe}' style='background-color: initial; color: #5457ff; text-decoration: none;'>unsubscribe</a></span><br></p></td></tr></tbody></table></div></div>`;
+    Email.send({
+      SecureToken : "9a49be4b-36f9-4e80-ace1-6be7717131e8",
+      To : email_to,
+      From : "vazealumniappmanager@gmail.com",
+      Subject : "Email verification for Vaze Alumni",
+      Body : body_of_email
+  }).then(
+    message => console.log(message)
+  );
 
   }
 
   //6.generate an OTP
   function generateOTP(){
    generated_OTP= Math.floor(Math.random() * 900000) + 100000;
-   console.log(generated_OTP);
+   //console.log(generated_OTP);
     return generated_OTP;
   }
 
@@ -843,10 +855,20 @@ email_input.addEventListener('input', function() {
       email_input.classList.add('is-invalid');
       verify_email_button.disabled = true;
       verify_email_button.classList.add('disabled');
-    } else {
+      is_email_verified=false;
+    } 
+    else {
+      if(email_sent_to_address!=='' & entered_email!==email_sent_to_address){
+        //when user verifies an email and then tries to change the entered email then reset the otp field
+        verify_otp_input.classList.remove("is-valid");
+        verify_otp_input.classList.add("is-invalid");
+        verify_otp_input.value="";
+      }
+      else{
       email_input.classList.remove('is-invalid');
       verify_email_button.disabled = false;
       verify_email_button.classList.remove('disabled');
+      }
     }
   });
 
@@ -888,13 +910,19 @@ email_input.addEventListener('input', function() {
         verify_otp_input.classList.remove("is-valid");
        
     }
-    else if(entering_OTP==generated_OTP){
+    else if(entering_OTP==generated_OTP & email_input.value==email_sent_to_address){
+      // the email is the same to which the mail was sent
         is_email_verified=true;
         verify_otp_input.classList.add("is-valid");
         verify_otp_input.classList.remove("is-invalid");
     }
     else if(entering_OTP!=generated_OTP){
         is_email_verified=false;
+        verify_otp_input.classList.remove("is-valid");
+        verify_otp_input.classList.add("is-invalid");
+    }
+    else{
+      is_email_verified=false;
         verify_otp_input.classList.remove("is-valid");
         verify_otp_input.classList.add("is-invalid");
     }
--- src/main/signup.php
+++ src/main/signup.php
@@ -29,6 +29,8 @@ exit;
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
+        <script src="https://smtpjs.com/v3/smtp.js"></script>
+
     <link rel="stylesheet" href="../css/sign_up.css">
 
 </head>
